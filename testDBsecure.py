d146da6d386ebdc5ed7b447536a017c2af37f004
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2024-03-15 18:22:49 +0900

    見やすく

--- testDBsecure.py
+++ testDBsecure.py
@@ -1,8 +1,8 @@
 import sqlite3 as sql
 
-dbname="test.db"
-conn=sql.connect(dbname)
-cur=conn.cursor()
+dbname = "test.db"
+conn = sql.connect(dbname)
+cur = conn.cursor()
 
 #cur.execute("CREATE TABLE users(user_id integer primary key autoincrement,user_name text NOT NULL,email text NOT NULL,pass text NOT NULL,location text NOT NULL,tel text NOT NULL)")
 #cur.execute("CREATE TABLE com(com_id integer primary key autoincrement,user_id integer NOT NULL,cont text NOT NULL)")
